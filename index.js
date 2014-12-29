45c6a0bf0a4d9d0a94f0cbdc7bee5fc243bed717
Mitch Weaver <mitchell.d.weaver@gmail.com>
2014-12-28 23:00:43 -0600

    starting to draw a grid

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 (function () {
-    console.log('hey');
-
     var canvas = document.getElementById('main'),
+        ctx = canvas.getContext('2d'),
         elemLeft = canvas.offsetLeft,
-        elemTop = canvas.offsetTop;
+        elemTop = canvas.offsetTop,
+        gridSize = 9,
+        gridElements = [];
+
+    function initialize () {
+        var totalGridElements = gridSize * gridSize;
+
+        for (var x = 0; x < gridSize; x++) {
+            for (var y = 0; y < gridSize; y++) {
+                gridElements.push({
+                    x: x,
+                    y: y
+                });
+            }
+        }
+    }
+
+    function drawGrid () {
+        var gridElementSize = 25,
+            initialOffsetX = 30,
+            initialOffsetY = 30,
+            initialXRender = initialOffsetX,
+            initialYRender = initialOffsetY * gridSize;
+
+            for (var x = 0; x < gridSize; x++) {
+                for (var y = 0; y < gridSize; y++) {
+                    ctx.strokeRect(initialXRender, initialYRender, gridElementSize, gridElementSize);
+                }
+            }
+    }
 
     canvas.addEventListener('click', function (event) {
         var x = event.pageX - elemLeft,
@@ -11,4 +39,7 @@
 
         console.log('x: ', x, 'y: ', y);
     });
+
+    initialize();
+    drawGrid();
 })();
