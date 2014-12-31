6de4a7823814e1fe3ae1cae480ccb2fbf201d6d5
Mitch Weaver <mitchell.d.weaver@gmail.com>
2014-12-31 13:53:07 -0600

    Make grid draw dynamic and fix coords

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
         elemLeft = canvas.offsetLeft,
         elemTop = canvas.offsetTop,
         gridSize = 9,
-        gridElements = [];
+        gridElements = [],
+        gridElementSize = 25,
+        initialOffsetX = 30,
+        initialOffsetY = 30;
 
     function initialize () {
         var totalGridElements = gridSize * gridSize;
@@ -12,25 +15,22 @@
         for (var x = 0; x < gridSize; x++) {
             for (var y = 0; y < gridSize; y++) {
                 gridElements.push({
-                    x: x,
-                    y: y
+                    gridX: x,
+                    gridY: y,
+                    physX: initialOffsetX + x * gridElementSize,
+                    physY: initialOffsetY + (gridSize - y) * gridElementSize
                 });
             }
         }
     }
 
     function drawGrid () {
-        var gridElementSize = 25,
-            initialOffsetX = 30,
-            initialOffsetY = 30,
-            initialXRender = initialOffsetX,
-            initialYRender = initialOffsetY * gridSize;
+        var gridElementsLength = gridElements.length;
 
-            for (var x = 0; x < gridSize; x++) {
-                for (var y = 0; y < gridSize; y++) {
-                    ctx.strokeRect(initialXRender, initialYRender, gridElementSize, gridElementSize);
-                }
-            }
+        for (var x = 0; x < gridElementsLength; x++) {
+            ctx.strokeRect(gridElements[x].physX, gridElements[x].physY, gridElementSize, gridElementSize);
+            ctx.fillText(gridElements[x].gridX + ',' + gridElements[x].gridY, gridElements[x].physX + 5, gridElements[x].physY + 10);
+        }
     }
 
     canvas.addEventListener('click', function (event) {
